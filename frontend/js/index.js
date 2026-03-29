663cdd47d67844b72056c91f2a083f1980ba4ca1
Boris Trunov <144445089+Borl1seTrunov@users.noreply.github.com>
2026-03-29 17:49:35 +0300

    add animations to text and add popup-function

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,11 +1,29 @@
+import { initAnimationText } from "./homePage/animationTextHomePage.js";
+import { createPopup } from "./popup.js";
+
+function initLogicChooseDatasetButton() {
+  const btn = document.querySelector('.section-datasets-choose__button--choice');
+  btn.addEventListener('click', () => {
+    window.location.href = '/datasets';
+  })
+}
+
 async function selectFolder() {
   const res = await fetch('/utils/select-folder');
   const { path } = await res.json();
-  if (path) {
-    alert(path);
-  }
+  if (!path) return;
+  return path;
 }
 
+async function uploadNewDataset() {
+  const path  = await selectFolder();
+  if (!path) return;
+  createPopup(`<h2>Заголовок</h2><p>${path}</p>`);
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('loadBtn').addEventListener('click', selectFolder);
+  document.getElementById('loadBtn').addEventListener('click', uploadNewDataset);
+  initAnimationText();
+  initLogicChooseDatasetButton();
 })
