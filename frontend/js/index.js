d4424a6daa5a66809cd87900a1e3cd44b36c41bc
Boris Trunov <144445089+Borl1seTrunov@users.noreply.github.com>
2026-03-29 21:01:49 +0300

    addNewDataset to module

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,5 +1,5 @@
 import { initAnimationText } from "./homePage/animationTextHomePage.js";
-import { createPopup } from "./popup.js";
+import { uploadNewDataset } from "./addNewDataset.js";
 
 function initLogicChooseDatasetButton() {
   const btn = document.querySelector('.section-datasets-choose__button--choice');
@@ -7,51 +7,6 @@ function initLogicChooseDatasetButton() {
     window.location.href = '/datasets';
   })
 }
-
-async function selectFolder() {
-  const res = await fetch('/utils/select-folder');
-  const { path } = await res.json();
-  if (!path) return;
-  return path;
-}
-
-async function uploadNewDataset() {
-  const path = await selectFolder();
-  if (!path) return;
-
-  createPopup(`
-    <h2 class="popup__title">Загрузка нового датасета</h2>
-    <p class="popup__sub">Выбранный путь: <span class="popup__path">${path}</span></p>
-    <div class="popup__field">
-      <label class="popup__label">Имя датасета:</label>
-      <input class="popup__input" id="datasetNameInput" placeholder="Введите имя датасета" />
-    </div>
-    <div class="popup__actions">
-      <button class="popup__btn popup__btn--addDataset">Продолжить</button>
-    </div>
-  `);
-
-  document.querySelector('.popup__btn--addDataset').addEventListener('click', async () => {
-    const name = document.getElementById('datasetNameInput').value.trim();
-    if (!name) {
-      document.getElementById('datasetNameInput').focus();
-      return;
-    }
-
-    const res = await fetch('http://localhost:8000/api/addDataset', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ dataset_name: name, path }),
-    });
-
-    if (!res.ok) {
-      return;
-    }
-
-    window.location.href = `/datasets`;
-  });
-}
-
 
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('loadBtn').addEventListener('click', uploadNewDataset);
